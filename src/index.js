16fb2f3c8b1b1de2896bdd3d98aa055960c0e046
Daniel Aziegbe <danielaziegbe98@gmail.com>
2025-10-13 13:22:47 -0400

    ADDED DYNAMIC SIDEBAR

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const toggleButton = document.getElementById("toggle-btn");
+const sidebar = document.getElementById("sidebar");
+
+function toggleSidebar() {
+   sidebar.classList.toggle("close");
+   closeSubMenus();
+}
+
+function toggleSubMenu(button) {
+   if (!button.nextElementSibling.classList.contains("show")) {
+      closeSubMenus();
+   }
+
+   button.nextElementSibling.classList.toggle("show");
+   button.classList.toggle("rotate");
+
+   if (sidebar.classList.contains("close")) {
+      sidebar.classList.toggle("close");
+   }
+}
+
+function closeSubMenus() {
+   Array.from(sidebar.getElementsByClassName("show")).forEach(ul => {
+      ul.classList.remove("show");
+      ul.previousElementSibling.classList.remove("rotate");
+   });
+}
--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,33 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+   entry: "./src/index.js",
+   output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: "./src/index.html",
+      }),
+   ],
+   module: {
+      rules: [
+         {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+         },
+         {
+            test: /\.html$/i,
+            use: "html-loader",
+         },
+         // Rule for image files (using Webpack 5 Asset Modules)
+         {
+            test: /\.(png|jpe?g|gif|svg|webp)$/i,
+            type: "asset/resource", // Copies the asset to the output directory
+         },
+      ],
+   },
 };
